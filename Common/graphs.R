0b9c314fbd73a491d2937110ea8d142948b2c4fc
Lance Lin <lancel@LANCEL-MACAIR.local>
2016-03-23 22:18:28 -0700

    adding graphs function and pkgs

---
+++ Common/graphs.R
@@ -0,0 +1,30 @@
+library(dygraphs)
+
+source("Common/load.data.R")
+
+# dygraphs application
+  #
+timeSeriesPlot <- function(dataInput, shinyInput){
+  
+}
+
+
+
+######################test########################
+
+hw <- HoltWinters(ldeaths)
+predicted <- predict(hw, n.ahead = 72, prediction.interval = TRUE)
+
+dygraph(predicted, main = "Predicted Lung Deaths (UK)") %>%
+  dyAxis("x", drawGrid = FALSE) %>%
+  dySeries(c("lwr", "fit", "upr"), label = "Deaths") %>%
+  dyOptions(colors = RColorBrewer::brewer.pal(3, "Set1"))
+
+lungDeaths <- cbind(mdeaths, fdeaths)
+dygraph(lungDeaths) %>%
+  dySeries("mdeaths", label = "Male") %>%
+  dySeries("fdeaths", label = "Female") %>%
+  dyOptions(stackedGraph = TRUE) %>%
+  dyRangeSelector(height = 20)
+
+######################test########################
